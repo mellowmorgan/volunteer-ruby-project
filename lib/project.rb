47fcb0dbe251814af1551037b5bba5c06eb7e8e3
mellowmorgan <morgan.waites@gmail.com>
2021-12-24 09:18:01 -0800

    add all method to Project test passing

--- lib/project.rb
+++ lib/project.rb
@@ -12,4 +12,17 @@ class Project
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first.fetch("id").to_i
   end
+
+  def ==(project_to_compare)
+    (self.title==project_to_compare.title)
+  end
+
+  def self.all
+    all_projects = []
+    results = DB.exec("SELECT * FROM projects;")
+    results.each do |entry|
+      all_projects.push(Project.new({ :title => entry.fetch("title"), :id => entry.fetch("id").to_i }))
+    end
+    all_projects
+  end
 end
