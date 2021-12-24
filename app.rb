3de3c051692c5aa489ba99c3b4b6afdd11c05211
mellowmorgan <morgan.waites@gmail.com>
2021-12-24 13:03:56 -0800

    update volunteer working

--- app.rb
+++ app.rb
@@ -37,10 +37,30 @@ get('/volunteers') do
 end
 
 post('/volunteers') do
-  # new_volunteer = Volunteer.new({:name => params[:volunteer], :project_id => params[:project].to_i, :id => nil })
   new_volunteer = Volunteer.new({:name => params[:volunteer], :project_id => params[:project].to_i, :id => nil })
   new_volunteer.save
   @volunteers = Volunteer.all
   @projects = Project.all
   erb(:volunteers)
 end
+
+get('/volunteers/:id') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  @projects = Project.all
+  if @volunteer.project_id == 0
+    @project_title = "none yet"
+  else
+    @project_title = Project.find(@volunteer.project_id).title
+  end
+  erb(:volunteer)
+end
+
+patch('/volunteers/:id') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  # @projects = Project.all
+  @volunteer.update({:name => params[:volunteer], :project_id => params[:project].to_i})
+  redirect '/volunteers'
+end
+
+
+
