75b9fc0a74073318aaee5d6ee184c362ecd58c4f
mellowmorgan <morgan.waites@gmail.com>
2021-12-24 09:36:34 -0800

    add all method to Volunteer class tests passing

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -17,4 +17,13 @@ class Volunteer
   def ==(volunteer_to_compare)
     (self.name == volunteer_to_compare.name) && (self.project_id == volunteer_to_compare.project_id)
   end
+
+  def self.all
+    all_volunteers = []
+    results = DB.exec("SELECT * FROM volunteers;")
+    results.each do |entry|
+      all_volunteers.push(Volunteer.new({ :name => entry.fetch("name"), :project_id => entry.fetch("project_id").to_i, :id => entry.fetch("id").to_i }))
+    end
+    all_volunteers
+  end
 end
