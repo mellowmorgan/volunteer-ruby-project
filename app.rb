5a8a8c357bc88f30afe2eaac7517d16a7e086dfc
mellowmorgan <morgan.waites@gmail.com>
2021-12-24 10:28:29 -0800

    add home routes and custom stylesheet

---
+++ app.rb
@@ -0,0 +1,17 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
+require 'pg'
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  erb(:home)
+end
+
+get('/home') do
+  erb(:home)
+end
